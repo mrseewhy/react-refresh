2f38286c6b9fa2fc0edf9e21cad5fc246e1aa264
Olusegun Adeyemi <53488450+mrseewhy@users.noreply.github.com>
2026-01-19 23:58:34 +0100

    Add the two pages to App

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -5,7 +5,7 @@ const Test = () => {
         <>
             <div className='flex justify-center items-center h-screen overflow-hidden'>
                 <div className='w-1/3 mx-auto h-[65vh] bg-[url("./src/assets/react.svg")] bg-no-repeat bg-[position:200%_center] bg-[length:70%_auto] bg-blend-overlay bg-gray-900 rounded-2xl shadow-md text-white text-3xl '>
-                    <div className='w-full h-1/6 bg-gray-950 rounded-t-2xl p-6'>
+                    <div className='w-full h-1/6 bg-gray-950 rounded-t-2xl p-6 shadow-sm shadow-blue-400'>
                         <div className='flex flex-row justify-between items-center'>
                             <div> <img src="./src/assets/react.svg" alt="" /></div>
                             <div>
