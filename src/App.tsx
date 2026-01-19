d7244bf41ac2d5626843978b8623aba25d6dabb7
Olusegun Adeyemi <53488450+mrseewhy@users.noreply.github.com>
2026-01-20 00:14:09 +0100

    changed color to cyan

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import Like from './components/Like'
 import Test from './components/Test'
--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -7,8 +7,8 @@ const Test = () => {
                 <div className='w-1/3 mx-auto h-[65vh] bg-[url("./src/assets/react.svg")] bg-no-repeat bg-[position:200%_center] bg-[length:70%_auto] bg-blend-overlay bg-gray-900 rounded-2xl shadow-md text-white text-3xl '>
                     <div className='w-full h-1/6 bg-gray-950 rounded-t-2xl p-6 shadow-sm shadow-blue-400'>
                         <div className='flex flex-row justify-between items-center'>
-                            <div> <img src="./src/assets/react.svg" alt="" /></div>
-                            <div>
+                            <div className='flex flex-row gap-2'> <img src="./src/assets/react.svg" alt="" /> <p className='text-2xl font-bold text-cyan-400'>ReactFacts</p></div>
+                            {/* <div>
                                 <nav>
                                     <ul className='text-sm flex flex-row gap-4'>
                                         <li>Home</li>
@@ -18,17 +18,17 @@ const Test = () => {
                                         <li>Contact</li>
                                     </ul>
                                 </nav>
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                     <div className='w-full h-5/6  p-8 mt-4'>
                         <p className='text-3xl font-bold'>Fun Facts About React!</p>
                         <ul className='text-sm mt-6 ml-4'>
-                            <li className='mb-2 list-disc marker:text-blue-500 marker:text-xl'>React is a JavaScript library for building user interfaces.</li>
-                            <li className='mb-2 list-disc marker:text-blue-500 marker:text-xl'>React is a JavaScript library for building user interfaces.</li>
-                            <li className='mb-2 list-disc marker:text-blue-500 marker:text-xl'>React is a JavaScript library for building user interfaces.</li>
-                            <li className='mb-2 list-disc marker:text-blue-500 marker:text-xl'>React is a JavaScript library for building user interfaces.</li>
-                            <li className='mb-2 list-disc marker:text-blue-500 marker:text-xl'>React is a JavaScript library for building user interfaces.</li>
+                            <li className='mb-2 list-disc marker:text-cyan-500 marker:text-xl'>React is a JavaScript library for building user interfaces.</li>
+                            <li className='mb-2 list-disc marker:text-cyan-500 marker:text-xl'>React is a JavaScript library for building user interfaces.</li>
+                            <li className='mb-2 list-disc marker:text-cyan-500 marker:text-xl'>React is a JavaScript library for building user interfaces.</li>
+                            <li className='mb-2 list-disc marker:text-cyan-500 marker:text-xl'>React is a JavaScript library for building user interfaces.</li>
+                            <li className='mb-2 list-disc marker:text-cyan-500 marker:text-xl'>React is a JavaScript library for building user interfaces.</li>
                         </ul>
                     </div>
 
